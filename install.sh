1e78e5ae08b506f4cfeb8e28978a6361d6ccb035
Thfirmin <thfirmin.dsantos@gmail.com>
2022-12-16 02:11:57 -0300

    add a TODO to dotfiles

---
+++ install.sh
@@ -0,0 +1,42 @@
+# Variables
+
+# Functions
+
+bash_init()
+{
+	if [[ -f ${HOME}/.bash_profile ]]
+	then
+		echo "We have a bash_profile"
+	else
+		if [[ -f ${HOME}/.profile ]]
+		then
+			echo "We have a profile"
+		else
+			echo "We don't have any profile"
+		fi
+	fi
+}
+
+zsh_init()
+{
+	echo $ZSH
+}
+
+# Verify Terminal Interpreter
+
+## Verify existece of Bash binary file
+if [ ${BASH} ]
+then
+	bash_init
+else
+	echo "Don't have bash"
+fi
+
+## Verify existece of Zsh binary file
+if [ ${ZSH} ]
+then
+	zsh_init
+else
+	echo "Don't have zsh"
+fi
+
